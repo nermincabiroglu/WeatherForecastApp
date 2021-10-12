0bed956bb62475ffc5f39b3c5aea605724887964
nerminkemanci <nermin.kemancii@gmail.com>
2021-10-12 16:53:56 +0300

    completed entity feature

--- app/src/main/java/com/kemanci/weatherforecast/model/entity/DailyWeatherReport.kt
+++ app/src/main/java/com/kemanci/weatherforecast/model/entity/DailyWeatherReport.kt
@@ -1,4 +1,65 @@
 package com.kemanci.weatherforecast.model.entity
 
-class DailyWeatherReport {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class DailyWeatherReport(
+    @SerializedName("dt")
+    var dt: Long,
+
+    @SerializedName("sunrise")
+    var sunrise: Long,
+
+    @SerializedName("sunset")
+    var sunset: Long,
+
+    @SerializedName("moonrise")
+    var moonrise: Long,
+
+    @SerializedName("moonset")
+    var moonset: Long,
+
+    @SerializedName("moon_phase")
+    var moonPhase: Double,
+
+    @SerializedName("temp")
+    var temp: Temperature,
+
+    @SerializedName("feels_like")
+    var feelsLike: FeelsLikeTemperature,
+
+    @SerializedName("pressure")
+    var pressure: Long,
+
+    @SerializedName("humidity")
+    var humidity: Long,
+
+    @SerializedName("dew_point")
+    var dewPoint: Double,
+
+    @SerializedName("wind_speed")
+    var windSpeed: Double,
+
+    @SerializedName("wind_deg")
+    var windDeg: Long,
+
+    @SerializedName("wind_gust")
+    var windGust: Double,
+
+    @SerializedName("weather")
+    var weather: List<Weather>,
+
+    @SerializedName("clouds")
+    var clouds: Long,
+
+    @SerializedName("pop")
+    var pop: Long,
+
+    @SerializedName("rain")
+    var rain: Double,
+
+    @SerializedName("uvi")
+    var uvi: Double
+):Parcelable
--- app/src/main/java/com/kemanci/weatherforecast/model/entity/WeatherReport.kt
+++ app/src/main/java/com/kemanci/weatherforecast/model/entity/WeatherReport.kt
@@ -1,4 +1,27 @@
 package com.kemanci.weatherforecast.model.entity
 
-class WeatherReport {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class WeatherReport (
+    @SerializedName("lat")
+    var lat: Double,
+
+    @SerializedName("lon")
+    var lon: Double,
+
+    @SerializedName("timezone")
+    var timezone: String,
+
+    @SerializedName("timezone_offset")
+    var timezoneOffset: Long,
+
+    @SerializedName("current")
+    var current: CurrentWeatherReport,
+
+    @SerializedName("daily")
+    var daily: List<DailyWeatherReport>
+
+):Parcelable
